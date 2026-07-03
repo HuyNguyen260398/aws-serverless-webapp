dccb70796ffff03f0b1ddb113ed419035312bb00
Huy Nguyen <huynguyen260398@gmail.com>
2026-07-04 04:28:14 +0700

    feat(infra): add lambda compute module

---
+++ infra/modules/compute/outputs.tf
@@ -0,0 +1,9 @@
+output "function_name" {
+  value = aws_lambda_function.todos.function_name
+}
+output "function_arn" {
+  value = aws_lambda_function.todos.arn
+}
+output "invoke_arn" {
+  value = aws_lambda_function.todos.invoke_arn
+}
